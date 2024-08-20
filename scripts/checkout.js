606576877b115202c6a199c0c014c6d243c2fd5f
OBA <ayo464003@gmail.com>
2024-08-20 13:39:27 +0100

    delete product from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -97,11 +98,15 @@ cart.forEach((cartItem) => {
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
 
-document.querySelector('.js-delete-link')
+document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
-    link.addEventLister('click', () => {
+    link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removedFromCart(productId);
-      console.log(cart);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     });
   });
